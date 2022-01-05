bb2f2de8183ebc3f8ba2517567798d5e715729a4
Jakub Švarc <jakubsvarc@protonmail.com>
2022-01-05 15:05:49 +0100

    feat: added main library class

---
+++ Protorduino.cpp
@@ -0,0 +1,35 @@
+#include "Protorduino.h"
+
+Protorduino::Protorduino(uint8_t numberOfTasks) : numberOfTasks(numberOfTasks) {
+    tasks = new Task[numberOfTasks];
+    tasksRegistrationStatus = new bool[numberOfTasks];
+    tasksDurations = new unsigned long[numberOfTasks];
+    lastExecutionTimestamps = new unsigned long[numberOfTasks];
+
+    for (uint8_t i = 0; i < numberOfTasks; i++) {
+        tasksRegistrationStatus[i] = false;
+    }
+}
+
+void Protorduino::registerTask(Task task, unsigned long tasksDuration, uint8_t order) {
+    tasks[order] = task;
+    tasksRegistrationStatus[order] = true;
+    tasksDurations[order] = tasksDuration;
+    lastExecutionTimestamps[order] = 0;
+}
+
+void Protorduino::execute() {
+    for (uint8_t i = 0; i < numberOfTasks; i++) {
+        if (tasksRegistrationStatus[i] == true) {
+            unsigned long currentTimestamp = millis();
+
+            if (lastExecutionTimestamps[i] + tasksDurations[i] <= currentTimestamp) {
+                tasks[i]();
+                
+                lastExecutionTimestamps[i] = currentTimestamp;
+            }
+        
+            break;
+        }
+    }
+}
---
+++ Protorduino.h
@@ -0,0 +1,22 @@
+#ifndef PROTORDUINO_H
+#define PROTORDUINO_H
+
+#include <Arduino.h>
+
+typedef void (*Task)();
+
+class Protorduino {
+  private:
+    uint8_t numberOfTasks;
+    Task* tasks;
+    bool* tasksRegistrationStatus;
+    unsigned long* tasksDurations;
+    unsigned long* lastExecutionTimestamps;
+
+  public:
+    Protorduino(uint8_t maxNumberOfTasks);
+    void registerTask(Task task, unsigned long tasksDuration, uint8_t order);
+    void execute();
+};
+
+#endif
